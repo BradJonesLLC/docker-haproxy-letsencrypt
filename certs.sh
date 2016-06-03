cee637b7f6b204db9772f05cc2406f44a85b4f0f
Brad Jones <brad@jones.name>
2016-06-03 17:13:26 -0600

    Combine certs into pem file consumable by haproxy

--- certs.sh
+++ certs.sh
@@ -4,4 +4,10 @@ letsencrypt-auto certonly --no-self-upgrade -n --text --standalone \
     --standalone-supported-challenges http-01 \
     -d "$CERTS" --keep --agree-tos --email "$EMAIL"
 
+for site in `ls -1 /etc/letsencrypt/live`; do
+cat /etc/letsencrypt/live/$site/privkey.pem \
+  /etc/letsencrypt/live/$site/fullchain.pem \
+  | tee /usr/local/etc/haproxy/certs/haproxy-"$site".pem >/dev/null
+done
+
 exit $?
